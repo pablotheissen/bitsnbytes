9695669a0137edd7a898c4f2ba62c908fd5ad628
Pablo Theissen <13626568+pablotheissen@users.noreply.github.com>
2020-04-20 22:33:43 +0200

    Inserted twig options to default config.example.php

--- config/config.example.php
+++ config/config.example.php
@@ -5,7 +5,19 @@ return [
     'date_formats' => [
         'short' => 'd.m.Y'
     ],
-    'database_dsn' => 'sqlite:' . __DIR__ . '/../data/bitsnbytes.sqlite',
+    'db' => [
+        'dsn' => 'sqlite:' . __DIR__ . '/../data/bitsnbytes.sqlite'
+    ],
     'timezone' => 'Europe/Berlin',
     'basepath' => '', // without trailing slash, '' for no basepath
+    'twig' => [
+        'paths' => [
+            __DIR__ . '/../src/templates',
+        ],
+        'options' => [
+            // Should be set to true in production
+            'cache_enabled' => false,
+//            'cache_path' => $settings['temp'] . '/twig',
+        ],
+    ],
 ];
